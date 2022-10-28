b985ac8c105e54a68f74490b3dab5499f60d2b58
IdanZakhaim <92606616+IdanZakhaim@users.noreply.github.com>
2022-10-28 12:48:45 +0300

    Add files via upload

---
+++ BiggersAvarage/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter tan numbers :");
+            final int ARRAY_SIZE = 10;
+            double sum = 0;
+            int[] tanNumbers = new int[ARRAY_SIZE];
+            for (int i = 0; i < ARRAY_SIZE; i++) {
+                tanNumbers[i] = scanner.nextInt();
+                sum = sum + (tanNumbers[i]);
+            }
+            double everage = sum / ARRAY_SIZE;
+            for (int i = 0; i < ARRAY_SIZE; i++) {
+                if (tanNumbers[i] > everage) {
+                    System.out.print(tanNumbers[i]);
+                }
+            }
+        }
+    }
